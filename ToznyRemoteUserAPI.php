1efa9d31d3b85c3540b6dc8d16c645a6bf5549d8
Greg Stromire <greg@tozny.com>
2016-03-17 16:15:01 -0700

    Add default empty strings to push credentials in loginResult

--- ToznyRemoteUserAPI.php
+++ ToznyRemoteUserAPI.php
@@ -136,8 +136,8 @@ class Tozny_Remote_User_API
             'user_id'      => $user['user_id'],
             'user_key_id'  => $user['user_key_id'],
             'session_id'   => $challenge['session_id'],
-            'push_token'   => $user['push_token'],
-            'push_platform'=> $user['push_platform']
+            'push_token'   => $user['push_token'] ?: '',    // not required
+            'push_platform'=> $user['push_platform'] ?: ''  // not required
         );
 
         $response = '';
